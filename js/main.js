b2cb771f0561136f9ecbcb57bc81330656e38cbf
tatevkalashyan <tatev.kalashyan91@gmail.com>
2021-01-29 18:19:46 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+// $(document).ready(function() {
+//     $('body').css('padding-bottom', $('footer').height() + 32);
+// });
+
+// $(window).resize(function(){
+//     $('body').css('padding-bottom', $('footer').height() + 32);
+// });
+
+// $(document).on("click","",function() {
+ 
+// }); 
